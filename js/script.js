aca42f3fdaabf11bc2295ffcb7ec7e38d9d3fb38
Iqbal Kang <iqbal.kang.cohort225@gmail.com>
2022-08-30 15:31:34 -0700

    before refactoring

--- js/script.js
+++ js/script.js
@@ -53,6 +53,10 @@ const quizNameBtn = get(".quiz__name-btn");
 const allQuestionsContainer = get(".questions__all");
 const quizesContainer = get(".available__quizes-box");
 const finishBtns = getAll(".btn--finish");
+const playQuestionTitle = get(".play__title");
+const playQuestionTime = get(".play__time");
+const playScore = get(".play__score");
+const playAnswers = getAll(".play__answer");
 
 class Quiz {
   constructor(qName) {
@@ -93,35 +97,65 @@ class Question {
     this.questionType = questionType;
     this.questionTime = questionTime;
     this.questionPoints = questionPoints;
+    this.questionId = Math.ceil(Math.random() * 100);
   }
 }
 
 class App {
   constructor() {
     this.currentQuizName = "";
+    this.score = 0;
+    this.currentQuestion = 0;
 
     this.quizes = [
       {
         tech: [
-          { question: "What is java?" },
-          { answer1: "A language" },
-          { answer2: "A compiler" },
-          { answer3: "An interpreter" },
-          { answer4: "A software" },
+          {
+            questionTitle: "What is java?",
+            answer1: "A language",
+            answer2: "A compiler",
+            answer3: "An interpreter",
+            answer4: "A software",
+            correctAnswer: "answer1",
+            questionType: "quiz",
+            questionTime: 10,
+            questionPoints: "standard",
+            questionId: 28,
+          },
+          {
+            questionTitle: "What is c++?",
+            answer1: "A software",
+            answer2: "A movie",
+            answer3: "A software",
+            answer4: "A place",
+            correctAnswer: "answer3",
+            questionTime: 5,
+            questionType: "trueorfalse",
+            questionPoints: "double",
+            questionId: 18,
+          },
         ],
       },
       {
         food: [
-          { question: "What is java?" },
-          { answer1: "A language" },
-          { answer2: "A compiler" },
-          { answer3: "An interpreter" },
-          { answer4: "A software" },
+          {
+            questionTitle: "What is java?",
+            answer1: "A language",
+            answer2: "A compiler",
+            answer3: "An interpreter",
+            answer4: "A software",
+            correctAnswer: "answer1",
+            questionType: "quiz",
+            questionPoints: "standard",
+            questionId: 28,
+          },
         ],
       },
     ];
 
+    // this.populateQuestions();
     // this.disbaleInputs();
+    this.fetchQuestion();
 
     quizNameBtn.addEventListener("click", () => {
       if (!quizNameInput.value) return;
@@ -145,7 +179,20 @@ class App {
         }
         this.addNewQuestion();
 
+        this.renderQuestions();
+
         this.reset();
+      });
+    });
+
+    allQuestionsContainer.addEventListener("click", (e) => {
+      this.deleteQuestion(e);
+      this.selectQuestion(e);
+    });
+
+    playAnswers.forEach((answer) => {
+      answer.addEventListener("click", (e) => {
+        this.checkAnswer(e);
       });
     });
 
@@ -162,6 +209,77 @@ class App {
         });
       });
     });
+  }
+
+  populateQuestions() {
+    let html = `
+    <div class="play__slide">
+        <div class="play__header">
+          <h2 class="play__title">What is java?</h2>
+
+          <div class="play__info">
+            <div class="play__timer-box">
+              <span class="play__time">20</span>
+            </div>
+            <h4>score: <span class="play__score">500 </span></h4>
+          </div>
+        </div>
+
+        <div class="play__answers">
+          <div class="play__answer play__answer--1">
+            <div class="play__shape play__shape--1"></div>
+            <p class="play__selected">Answer</p>
+          </div>
+
+          <div class="play__answer play__answer--2">
+            <div class="play__shape play__shape--2"></div>
+            <p class="play__selected">Answer</p>
+          </div>
+
+          <div class="play__answer play__answer--3">
+            <div class="play__shape play__shape--3"></div>
+            <p class="play__selected">Answer</p>
+          </div>
+
+          <div class="play__answer play__answer--4">
+            <div class="play__shape play__shape--4"></div>
+            <p class="play__selected">Answer</p>
+          </div>
+        </div>
+      </div>
+    `;
+    containerPlay.insertAdjacentHTML("beforeend", html);
+  }
+
+  fetchQuestion() {
+    let questionsData = this.quizes[0].tech;
+
+    playQuestionTime.textContent =
+      questionsData[this.currentQuestion].questionTime;
+    playScore.textContent = this.score;
+    playQuestionTitle.textContent =
+      questionsData[this.currentQuestion].questionTitle;
+    playAnswers.forEach((answer, index) => {
+      if (index == 0)
+        answer.textContent = questionsData[this.currentQuestion].answer1;
+      if (index == 1)
+        answer.textContent = questionsData[this.currentQuestion].answer2;
+      if (index == 2)
+        answer.textContent = questionsData[this.currentQuestion].answer3;
+      if (index == 3)
+        answer.textContent = questionsData[this.currentQuestion].answer4;
+    });
+  }
+
+  checkAnswer(e) {
+    let questionsData = this.quizes[0].tech;
+    const selectedAnswer = e.target.dataset.selected;
+
+    if (selectedAnswer === questionsData[this.currentQuestion].correctAnswer) {
+      this.score += 100;
+      this.currentQuestion++;
+      this.fetchQuestion();
+    }
   }
 
   displayQuiz(quizName, questions) {
@@ -216,8 +334,12 @@ class App {
   }
 
   radioInputValidator() {
-    const radioInputs = [...getAll("input[type='radio']")];
-    return radioInputs.some((input) => input.checked);
+    try {
+      const radio = get('input[type="radio"]:checked');
+      return radio.checked;
+    } catch {
+      return false;
+    }
   }
 
   addNewQuestion() {
@@ -227,7 +349,7 @@ class App {
 
     const selects = getAll("select");
 
-    const radioInputs = [...getAll("input[type='radio']")];
+    const selectedRadio = get('input[type="radio"]:checked');
 
     let questionTitle,
       answer1,
@@ -239,8 +361,7 @@ class App {
       questionTime,
       questionPoints;
 
-    correctAnswer = radioInputs.filter((radioInput) => radioInput.checked)[0]
-      .value;
+    correctAnswer = selectedRadio.value;
 
     inputs.forEach((input, index) => {
       if (index === 0) questionTitle = input.value;
@@ -268,23 +389,34 @@ class App {
       questionPoints
     );
 
-    this.displayQuestion(question);
-    console.log(this.quizes);
+    this.currentQuizArr().push(question);
 
+    // return question;
+
+    console.log(this.quizes);
+  }
+
+  currentQuizArr() {
     const currentQuizIndex = this.quizes.findIndex((quiz) => {
       for (let obj in quiz) return obj === this.currentQuizName;
     });
 
-    this.quizes[currentQuizIndex][this.currentQuizName].push(question);
-
-    return question;
+    return this.quizes[currentQuizIndex][this.currentQuizName];
   }
 
-  displayQuestion({ questionTitle, questionTime }) {
+  renderQuestions() {
+    allQuestionsContainer.textContent = "";
+
+    this.currentQuizArr().forEach((question, index) => {
+      this.displayQuestion(question, index);
+    });
+  }
+
+  displayQuestion({ questionTitle, questionTime, questionId }, index) {
     let html = `
     <div class="questions__box">
           <div class="questions__heading">
-            <span class="questions__number">1</span>
+            <span class="questions__number">${index + 1}</span>
             <p>Quiz</p>
           </div>
           <div class="question__info">
@@ -296,19 +428,45 @@ class App {
             <div class="question__timer-box">
               <span class="question__timer">${questionTime}</span>
             </div>
-            <i class="fa-solid fa-trash-can question__delete"></i>
+            <i class="fa-solid fa-trash-can question__delete" data-id="${questionId}"></i>
           </div>
         </div>`;
-
     allQuestionsContainer.insertAdjacentHTML("beforeend", html);
+  }
+
+  deleteQuestion(e) {
+    if (e.target.classList.contains("question__delete")) {
+      const deleteQuestionId = +e.target.dataset.id;
+
+      const deleteQuestionIndex = this.currentQuizArr().findIndex(
+        (question) => question.questionId === deleteQuestionId
+      );
+
+      this.currentQuizArr().splice(deleteQuestionIndex, 1);
+
+      e.target.parentElement.parentElement.remove();
+
+      this.currentQuizArr().forEach((question) =>
+        this.renderQuestions(question)
+      );
+    }
+  }
+
+  selectQuestion(e) {
+    if (e.target.closest(".questions__box")) {
+      //work later
+    }
   }
 
   reset() {
     const inputs = getAll("input[type = 'text']");
     const selects = getAll("select");
-    // const selectedRadio = get('input[type="radio"]:checked');
+    const selectedRadio = get('input[type="radio"]:checked');
+
     inputs.forEach((input) => (input.value = ""));
-    // selectedRadio.checked = false;
+
+    selectedRadio.checked = false;
+
     answerInputs.forEach((inputEl) => {
       changeAnswerInputBg(inputEl, "#fff", "none");
     });
@@ -316,33 +474,3 @@ class App {
 }
 
 let app = new App();
-
-// addQuestion() {
-//   quiz1.tech.push(new Question(this.question));
-//   console.log("kk");
-// }
-
-// class QuizTry {
-//   constructor(qName) {
-//     this[qName] = [];
-//   }
-// }
-
-// class QuestionTry {
-//   constructor(q, a1, a2, a3, a4) {
-//     this.q = q;
-//     this.a1 = a1;
-//     this.a2 = a2;
-//     this.a3 = a3;
-//     this.a4 = a4;
-//   }
-// }
-
-// let quiz1 = new QuizTry("tech");
-// let quiz2 = new QuizTry("food");
-
-// quiz1.tech.push(new Question("bWhat is my Name?", "bala", "20", "jal", "dev"));
-// quiz1.tech.push(new Question("jeeto"));
-
-// console.log(quiz1);
-// console.log(quiz2);
